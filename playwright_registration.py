07c0bd50b884af4b25073e587f6acee27f530739
nodleSsh <gelena33dmitrieva@yandex.ru>
2026-02-12 21:45:50 +0300

    autorization

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,25 +1,29 @@
-
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    email_input.fill("user.name@gmail.com")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('poppu@gmail.com')
 
-    username_input = page.get_by_test_id("registration-form-username-input").locator("input")
-    username_input.fill("username")
+    user_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    user_input.fill('poppy')
 
-    password_input = page.get_by_test_id("registration-form-password-input").locator("input")
-    password_input.fill('password')
+    pass_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    pass_input.fill('poppy')
 
-    registration_button = page.get_by_test_id("registration-page-registration-button")
-    registration_button.click()
+    reg_button = page.get_by_test_id('registration-page-registration-button')
+    reg_button.click()
 
-    dashboard = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashboard).to_be_visible()
-    # page.wait_for_timeout(5000)
+    context.storage_state(path='brwsr-state.json')
 
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='brwsr-state.json')
+    page = context.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
 
+    page.wait_for_timeout(5000)
